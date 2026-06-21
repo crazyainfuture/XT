324edc9be469fc4c77ae600c94ddd6aefa0f7b10
XT <ozymmadeinfuture@gmail.com>
2026-06-21 19:39:17 +0800

    修改筆記連結

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,7 +1,7 @@
 export const allProjects = [
   { id: 0, title: 'my-portfolio', desc: '我的個人網站', link: 'https://crazyainfuture.github.io/XT/' },
   { id: 1, title: 'FJU EAT', desc: '輔大校園美食地圖', link: 'https://crazyainfuture.github.io/fjufindfood/' },
-  { id: 2, title: 'brain.push_back', desc: '大學筆記、作業、程式相關資訊，歡迎挖寶!', link: 'https://github.com/crazyainfuture/brain.push_back.git' },
+  { id: 2, title: 'XT\'s Journey', desc: '大學筆記、作業、程式相關資訊，歡迎挖寶!', link: 'https://crazyainfuture.github.io/XT.push_back.github.io/' },
   { id: 3, title: 'medical_platform', desc: '一個結合AI與醫療的平台，學習api串接與網頁開發', link: 'https://github.com/crazyainfuture/medical_platform_revised.git' },
   { id: 4, title: 'GateAway', desc: '《GateAway》是一款以「環狀無限循環跑道」為核心的反應型駕駛生存街機遊戲，與朋友一起開發', link: 'https://github.com/shyuray/GateAway.git' },
   { id: 5, title: 'Crown', desc: '《平民王冠》是一款以「從平凡到榮耀」為主題的遊戲作品。玩家將扮演一位平民，透過努力、抉擇與冒險，最終爭奪屬於自己的王冠。' },
